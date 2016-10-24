9fccf3af577945edbcc47f9404f8962585d81742
Sergio111 <sergallor@hotmail.com>
2016-10-24 17:42:40 +0200

    kicker ajusted

--- Nes_Pinball/ModuleSceneIntro.cpp
+++ Nes_Pinball/ModuleSceneIntro.cpp
@@ -371,7 +371,7 @@ bool ModuleSceneIntro::Start()
 	circles.getLast()->data->listener = this;
 
 	paddlesL.add(App->physics->CreatePaddleL(108, 429, (40 * DEGTORAD), -30 * DEGTORAD, 0x0001, 0x0001 | 0x0008));
-	paddlesL.add(App->physics->CreatePaddleL(109, 204, (40 * DEGTORAD), -30 * DEGTORAD, 0x0001, 0x0001 | 0x0008));
+	paddlesL.add(App->physics->CreatePaddleL(109, 201, (40 * DEGTORAD), -30 * DEGTORAD, 0x0001, 0x0001 | 0x0008));
 	paddlesR.add(App->physics->CreatePaddleR(180, 430, (146 * DEGTORAD), 78 * DEGTORAD, 0x0001, 0x0001 | 0x0008));
 	paddlesR.add(App->physics->CreatePaddleR(179, 203, (146 * DEGTORAD), 78 * DEGTORAD, 0x0001, 0x0001 | 0x0008));
 
